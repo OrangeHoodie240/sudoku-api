6e72351f0a55e2d74fe826938884e237500a38ba
steven daddario <daddarios@dupage.edu>
2021-09-06 05:30:56 -0500

    adding cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express'); 
 const sudokuRoutes = require('./routes/sudokuRoutes');
-
+const cors = require('cors');
 
 const app = express(); 
 
+app.use(cors({origin: '*'}));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true})); 
