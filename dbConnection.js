a2ad0428512fea925aa999140f636e410a9848a5
steven daddario <daddarios@dupage.edu>
2021-09-09 22:23:01 -0500

    attempting to create db structure on server

--- dbConnection.js
+++ dbConnection.js
@@ -1,10 +1,9 @@
 const {Client} = require('pg'); 
 
 
-let DATABASE_URI = process.env.DATABASE_URI || 'postgresql:///sudoku';
+let DATABASE_URI = process.env.DATABASE_URL || 'postgresql:///sudoku';
 if(process.NODE_ENV==='test') DATABASE_URI = 'postgresql:///sudoku';
 
-console.log(DATABASE_URI);
 const dbConnection = new Client({connectionString: DATABASE_URI});
 dbConnection.connect(); 
 
