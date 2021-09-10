a88926c0e2cb6ddabb7bb37df4b1f77aa81b8479
steven daddario <daddarios@dupage.edu>
2021-09-09 22:11:25 -0500

    attempting to create db structure on server

---
+++ seedDatabase.js
@@ -0,0 +1,17 @@
+const dbConnection = require('./dbConnection');
+
+
+
+
+await dbConnection.query(`CREATE TABLE sudoku_users(
+                            id SERIAL PRIMARY KEY, 
+                            email VARCHAR(64) NOT NULL UNIQUE,  
+                            password VARCHAR(255) NOT NULL
+    )`); 
+
+await dbConnection.query(`CREATE TABLE sudoku_incomplete_puzzles(
+                            user_id INTEGER REFERENCES sudoku_users(id) ON DELETE CASCADE, 
+                            level VARCHAR(10) NOT NULL, 
+                            puzzle_id INT UNIQUE NOT NULL, 
+                            puzzle CHAR(162) NOT NULL UNIQUE
+                        )`);
