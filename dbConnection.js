95c0dfcd74a931289edce3b36c0367d35570a14b
steven daddario <daddarios@dupage.edu>
2021-09-09 22:58:17 -0500

    attempting to create db structure on server

--- dbConnection.js
+++ dbConnection.js
@@ -3,8 +3,14 @@ const {Client} = require('pg');
 
 let DATABASE_URI = process.env.HEROKU_POSTGRESQL_BLUE_URL || 'postgresql:///sudoku';
 if(process.NODE_ENV==='test') DATABASE_URI = 'postgresql:///sudoku';
-console.log(DATABASE_URI);
-const dbConnection = new Client({connectionString: DATABASE_URI});
+
+const dbConnection = new Client({
+                                    connectionString: DATABASE_URI, 
+                                    ssl: {
+                                        rejectUnauthorized: false
+                                    }
+});
+
 dbConnection.connect(); 
 
 module.exports = dbConnection; 
