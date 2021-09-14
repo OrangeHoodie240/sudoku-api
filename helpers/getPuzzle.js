a7620e2ea96bb3b164a975826723ad28b0559211
steven daddario <daddarios@dupage.edu>
2021-09-14 04:54:25 -0500

    puzzle changes

--- helpers/getPuzzle.js
+++ helpers/getPuzzle.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 function getPuzzle(level){
-    let fileName = './puzzles/level_' + level +'_100_1.json';
+    let fileName = './puzzles/level_' + level +'.json';
     let data = fs.readFileSync(fileName, 'utf-8');
     data = JSON.parse(data); 
     let length = data.length; 
